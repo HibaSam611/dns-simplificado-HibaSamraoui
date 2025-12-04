7b94e8a9062165780271e9cd2f823fde621d0fe9
HibaSam611 <hibasamraoui17@gmail.com>
2025-12-04 12:49:02 +0100

    Fase 2 corregida de la practica

--- src/ServidorDNS.java
+++ src/ServidorDNS.java
@@ -10,23 +10,18 @@ public class ServidorDNS {
         HashMap<String, String> baseDNS = new HashMap<>();
 
         try {
-            // Abrimos dns.txt para leer los registros
             BufferedReader br = new BufferedReader(new FileReader("dns.txt"));
-
             String linea;
 
-            // Leemos cada línea del archivo
             while ((linea = br.readLine()) != null) {
-
                 String[] partes = linea.split(" ");
 
-                String dominio = partes[0]; // google.com
-                String tipo = partes[1];    // A
-                String ip = partes[2];   // 172.217.17.4
+                String dominio = partes[0];
+                String tipo = partes[1];
+                String valor = partes[2];
 
-                // En fase 1 SOLO guardamos los de registro A
                 if (tipo.equals("A")) {
-                    baseDNS.put(dominio, ip);
+                    baseDNS.put(dominio, valor);
                 }
             }
 
@@ -47,7 +42,6 @@ public class ServidorDNS {
             System.out.println("Cliente conectado.");
 
             BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-
             PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
 
             String comando;
@@ -58,57 +52,55 @@ public class ServidorDNS {
 
                     if (comando.equalsIgnoreCase("EXIT")) {
                         salida.println("200 Bye");
-                        break;
+                        break; // salir del bucle
                     }
 
-                    if (comando.startsWith("LOOKUP")) {
-                        String[] partes = comando.split(" ");
+                    // LOOKUP
+                    else if (comando.startsWith("LOOKUP")) {
+
+                        String[] partes = comando.split("\\s+");
                         if (partes.length != 3) {
                             salida.println("400 Bad request");
                             continue;
                         }
 
-                        String tipo = partes[1];     // "A"
-                        String dominio = partes[2];  //"google.com"
+                        String tipo = partes[1];
+                        String dominio = partes[2];
 
-                        // En la fase 1 solo aceptamos A
                         if (!tipo.equals("A")) {
                             salida.println("400 Bad request");
                             continue;
                         }
 
                         if (baseDNS.containsKey(dominio)) {
-                            String ip = baseDNS.get(dominio);
-                            salida.println("200 " + ip);  // encontrado
+                            salida.println("200 " + baseDNS.get(dominio));
                         } else {
-                            salida.println("404 Not Found"); //no existe
+                            salida.println("404 Not Found");
                         }
-
-
                     }
-                    if (comando.equalsIgnoreCase("LIST")) {
 
-                        // Inicio del listado
+                    // LIST
+                    else if (comando.equalsIgnoreCase("LIST")) {
+
                         salida.println("150 Inicio listado");
 
-                        // recorremos el hashmap
                         for (String dominio : baseDNS.keySet()) {
                             String ip = baseDNS.get(dominio);
-
                             salida.println(dominio + " A " + ip);
                         }
 
-                        // Fin del listado
                         salida.println("226 Fin listado");
-                        continue;
                     }
 
-                    salida.println("400 Bad request");
+                    else {
+                        salida.println("400 Bad request");
+                    }
 
                 } catch (Exception e) {
                     salida.println("500 Server error");
                 }
             }
+
             cliente.close();
             System.out.println("Cliente desconectado.");
 
@@ -117,3 +109,4 @@ public class ServidorDNS {
         }
     }
 }
+
